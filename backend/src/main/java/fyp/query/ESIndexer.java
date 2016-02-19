62c827e8e6d20fed3eefc7c9404c834a8dc0794f
Phuc Nguyen <phuc10cdt2@gmail.com>
2016-02-19 13:18:31 +0800

    Added bulk index to ES

--- backend/src/main/java/fyp/query/ESIndexer.java
+++ backend/src/main/java/fyp/query/ESIndexer.java
@@ -7,11 +7,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 // import java.net.MalformedURLException;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONValue;
@@ -60,6 +62,7 @@ public class ESIndexer {
             System.out.println(obj.toJSONString());
             StringEntity input = new StringEntity(obj.toJSONString());
             input.setContentType("application/json");
+            System.out.println(input);
             putReq.setEntity(input);
             HttpResponse response = httpClient.execute(putReq);
             if(response.getStatusLine().getStatusCode() != 201){
@@ -77,9 +80,71 @@ public class ESIndexer {
             e.printStackTrace();
         }
     }
-    public static void index (List <Patent> patents, String esURL){
-        for(Patent p: patents){
-            index(p, esURL);
+    public static String createIndex(Patent p){
+        JSONObject obj = new JSONObject();
+        obj.put("title", p.getTitle());
+        obj.put("abstract", p.getAbstract());
+        obj.put("text", p.getText());
+        obj.put("claims", p.getClaims());
+        obj.put("date", p.getDate().toString());
+        JSONArray companyList = new JSONArray();
+        if(p.getCompany() != null){
+            String [] companies = p.getCompany().split(";");
+            for(int i = 0; i<companies.length; i++){
+                companyList.add(companies[i]);
+            }
         }
+        obj.put("company", companyList);
+        JSONArray inventorList = new JSONArray();
+        if(p.getInventor() != null){
+            String [] inventors = p.getInventor().split(";");
+            for(int i = 0; i<inventors.length; i++){
+                inventorList.add(inventors[i]);
+            }
+        }
+        obj.put("inventors", inventorList);
+        obj.put("city", p.getCity());
+        obj.put("country", p.getCountry());
+        JSONArray ipcList = new JSONArray();
+        if(p.getIPC() != null){
+            String [] ipcs = p.getIPC().split(";");
+            for(int i = 0; i<ipcs.length; i++){
+                ipcList.add(ipcs[i]);
+            }
+        }
+        obj.put("ipcs", ipcList);
+        return obj.toJSONString();
+    }
+    public static void index (List <Patent> patents, String esURL){
+        String jsonBulk = "";
+        for(Patent p: patents){
+//            index(p, esURL);
+            jsonBulk = jsonBulk + "{\"index\":{\"_id\":" + "\"" + p.getDocId().toString() + "\"" + "}" + "\n";
+            jsonBulk += createIndex(p);
+            jsonBulk += "\n";
+        }
+        try{
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpPut putReq = new HttpPut(esURL + "_bulk");
+            StringEntity input = new StringEntity(jsonBulk);
+            input.setContentType("application/json");
+            System.out.println(input);
+            putReq.setEntity(input);
+            HttpResponse response = httpClient.execute(putReq);
+            if(response.getStatusLine().getStatusCode() != 201 && response.getStatusLine().getStatusCode() != 200){
+                System.out.println("Post request failed");
+                System.out.println(response);
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            String output;
+            while((output = br.readLine())!= null){
+                System.out.println(output);
+            }
+            httpClient.getConnectionManager().shutdown();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(jsonBulk);
     }
 }
